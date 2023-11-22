a871023c4da9004a4c3793cc0384e449a8e25a54
Magnus Kjellberg <magnus.kjellberg@columbusglobal.com>
2023-11-22 19:43:56 +0100

    Fixed login_form

--- src/login.rs
+++ src/login.rs
@@ -13,8 +13,14 @@ use crate::error::Result;
 use crate::templates::HtmlTemplate;
 
 #[derive(Template)]
-#[template(path = "user_input.html")]
+#[template(path = "login_page.html")]
 pub struct UserInputTemplate {}
+
+#[derive(Template)]
+#[template(path = "login_error.html")]
+pub struct LoginErrorForm {
+    pub error_msg: String,
+}
 
 #[derive(Deserialize)]
 pub struct LoginForm {
@@ -23,6 +29,7 @@ pub struct LoginForm {
 
 pub async fn post(
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
+    State(pool): State<PgPool>,
     Form(login_form): Form<LoginForm>,
 ) -> Result<impl IntoResponse> {
     println!(
@@ -30,11 +37,20 @@ pub async fn post(
         addr.ip().to_string(),
         login_form.username
     );
-    return Err(crate::error::Error::NotFound);
+
+    let login_error_form = LoginErrorForm {
+        error_msg: String::from("Testing"),
+    };
+
+    /*SELECT * FROM users
+    WHERE name = ${username.toString()}
+    ORDER BY id ASC LIMIT 100 */
+    return Ok(HtmlTemplate(login_error_form).into_response());
+    //return Err(crate::error::Error::NotFound);
     let mut headers = HeaderMap::new();
     headers.append("HX-Redirect", "/7/question/12".parse().unwrap());
     //HX-Redirect: /question/xx
-    return Ok((headers, "Should be redirected"));
+    return Ok((headers, "Should be redirected").into_response());
 }
 
 pub async fn get() -> Result<impl IntoResponse> {
